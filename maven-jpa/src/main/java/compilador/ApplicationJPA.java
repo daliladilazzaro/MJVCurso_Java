9ab08db65ca2a71c0b89d6ca3465e381a2efffd8
daliladilazzaro <dilazzaro06@gmail.com>
2021-06-27 14:05:12 -0300

    adicionado remove list e alterar para a classes de jpa e atualização do arquivo .sql

--- maven-jpa/src/main/java/compilador/ApplicationJPA.java
+++ maven-jpa/src/main/java/compilador/ApplicationJPA.java
@@ -13,17 +13,11 @@ import utils.FormatadorUtil;
  */
 public class ApplicationJPA {
 
-    static CadastroJpaDAO cadDAO = new CadastroJpaDAO();
+    static CadastroJpaDAO cjpa = new CadastroJpaDAO();
 
     public static void main(String[] args) {
-        testeCadastroJPA();
-    }
-
-    private static void testeCadastroJPA() {
-//        EntityManager em = ConexaoJPA.criarConexao();
-        CadastroJpaDAO cjpa = new CadastroJpaDAO();
         Cadastro cadModel = new Cadastro();
-
+//        cadModel.setId(2);
         cadModel.setPf_pj("F");
         cadModel.setNome("Dalila Di Lazzaro");
         cadModel.setCpf_cnpj("25478697530");
@@ -40,8 +34,50 @@ public class ApplicationJPA {
         cadModel.setBairro("Jardins");
         cadModel.setSite_instagram("@dilazzaro");
         cadModel.setObservacao("Gosto de tocar violao.");
-        cadModel.setNome("Jos Maia");
-        cjpa.incluir(cadModel);
+//        testeCadastroJPA(cadModel);
+//        testeAlterarJPA(cadModel);
+//        testeAlterarJPA();
+//        testeRemoverJPA();
+//        testeBusca();
     }
 
+    private static void testeRemoverJPA() {
+        Cadastro cadModel = new Cadastro();
+        cadModel.setId(1);
+        cjpa.remover(cadModel);
+    }
+
+    private static void testeAlterarJPA(Cadastro c) {        
+        cjpa.alterar(c);
+    }
+
+    private static void testeCadastroJPA(Cadastro c) {
+//        EntityManager em = ConexaoJPA.criarConexao();      
+        cjpa.incluir(c);
+    }
+
+    public static void testeBusca() {
+        List<Cadastro> l = cjpa.busca();
+        for (int i = 0; i < l.size(); i++) {
+            Cadastro cad = l.get(i);
+            System.out.println("id: " + cad.getId());
+            System.out.println("pf_pj: " + cad.getPf_pj());
+            System.out.println("nome: " + cad.getNome());
+            System.out.println("Cpf_cnpj: " + FormatadorUtil.FormataCpfCnpj(cad.getCpf_cnpj()));
+            System.out.println("telefone1: " + FormatadorUtil.FormataTelefone(cad.getTelefone1().toString()));
+            System.out.println("telefone2: " + FormatadorUtil.FormataTelefone(cad.getTelefone2().toString()));
+            System.out.println("email: " + cad.getEmail());
+            System.out.println("area_atuacao: " + cad.getArea_atuacao());
+            System.out.println("cep: " + FormatadorUtil.FormataCEP(cad.getCep()));
+            System.out.println("estado: " + cad.getEstado());
+            System.out.println("cidade: " + cad.getCidade());
+            System.out.println("rua: " + cad.getRua());
+            System.out.println("numero: " + cad.getNumero());
+            System.out.println("complemento: " + cad.getComplemento());
+            System.out.println("bairro: " + cad.getBairro());
+            System.out.println("site_instagram: " + cad.getSite_instagram());
+            System.out.println("observacao: " + cad.getObservacao());
+            System.out.println("---------------------------");
+        }
+    }
 }
--- maven-jpa/src/main/java/dao/CadastroJpaDAO.java
+++ maven-jpa/src/main/java/dao/CadastroJpaDAO.java
@@ -1,5 +1,6 @@
 package dao;
 
+import java.util.List;
 import javax.persistence.EntityManager;
 import jpa.ConexaoJPA;
 import model.Cadastro;
@@ -19,7 +20,44 @@ public class CadastroJpaDAO {
     public void incluir(Cadastro c) {
         em.getTransaction().begin();
         em.persist(c);
-//        em.merge(c);//ele altera
         em.getTransaction().commit();
+    }
+
+    public void alterar(Cadastro c) {
+        try {
+            em.getTransaction().begin();
+            em.merge(c);//ele altera
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            em.getTransaction().rollback();
+        }
+    }
+    public List<Cadastro> busca(){
+        return em.createQuery("FROM "+ Cadastro.class.getName()).getResultList();
+    }
+    public void remover(Cadastro c) {
+        try {
+            em.getTransaction().begin();
+            c = em.find(Cadastro.class, c.getId());
+            em.remove(c);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            em.getTransaction().rollback();
+        }
+    }
+    
+    public void removerById(final int id){
+        try {
+            Cadastro c = getById(id);
+            remover(c);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public Cadastro getById(final int id){
+        return em.find(Cadastro.class, id);
     }
 }
